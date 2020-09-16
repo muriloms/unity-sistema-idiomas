4b7b2949ede206610686dd7347c42af696908674
unknown <ms7murilo@gmail.com>
2020-09-16 18:52:57 -0300

    first and final commit

---
+++ SistemasMultiplosIdiomas/Assets/GameText/LoadXMLFile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+namespace GameText
+{
+    public class LoadXMLFile
+    {
+        
+        public List<string> LoadXMLData()
+        {
+            
+            string idioma = "pt-br";
+            if (PlayerPrefs.GetString("idioma") != "")
+            {
+                idioma = PlayerPrefs.GetString("idioma");
+            }
+            
+            string arquivoXML = "language";
+            
+            TextAsset xmlData = (TextAsset) Resources.Load(idioma + "/" + arquivoXML);
+            XmlDocument _xmlDocument = new XmlDocument();
+        
+            _xmlDocument.LoadXml(xmlData.text);
+            
+            List<string> textoTelaInicial = new List<string>();
+            foreach (XmlNode node in _xmlDocument["language"].ChildNodes)
+            {
+                string nodeName = node.Attributes["name"].Value;
+                
+                foreach (XmlNode n in node["textos"].ChildNodes)
+                {
+                    textoTelaInicial.Add(n.InnerText);
+                }
+            }
+            Debug.Log(textoTelaInicial.Count);
+            return textoTelaInicial;
+
+        }
+    }
+}
+
---
+++ SistemasMultiplosIdiomas/Assets/Script/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameText;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private LoadXMLFile _loadXMLFile;
+
+    [SerializeField] private Text textNomeJogo;
+    [SerializeField] private Text textInformacoesJogo;
+    [SerializeField] private Text textCreditos;
+
+    private List<string> _textoTelaInicial;
+    private void Awake()
+    {
+        TextosTelaInicial();
+    }
+
+    private void TextosTelaInicial()
+    {
+        _loadXMLFile = new LoadXMLFile();
+        _textoTelaInicial = _loadXMLFile.LoadXMLData();
+        textNomeJogo.text = _textoTelaInicial[0];
+        textInformacoesJogo.text = _textoTelaInicial[1];
+        textCreditos.text = _textoTelaInicial[2];
+    }
+
+    public void TrocarIdioma(string idioma)
+    {
+        PlayerPrefs.SetString("idioma", idioma);
+        TextosTelaInicial();
+    }
+}
